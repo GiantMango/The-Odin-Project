5196a5cfe1260ee2dbbb734a1ab5db9f793b5a0c
GiantMango <subway1101@gmail.com>
2024-11-10 17:11:05 -0800

    Add DOM and UI

--- projects/rock-paper-scissors/script.js
+++ projects/rock-paper-scissors/script.js
@@ -1,47 +1,66 @@
-console.log("Let's play rock paper scissors!")
 
 let humanScore = 0;
 let computerScore = 0;
 
+const paperImg = "https://cdn11.bigcommerce.com/s-2i5mq6440u/images/stencil/2048x2048/products/3762/9095/PlasticPaper-CutSheet__18809.1597757191.png?c=2";
+const scissorsImg = "https://www.ikea.com/ca/en/images/products/svartfisk-kitchen-scissors-stainless-steel-black__1215188_pe911902_s5.jpg?f=xl";
+const rockImg = "https://cdn-hgmij.nitrocdn.com/uwlPXoysYPxhfaCezxmZtaumClejIefR/assets/images/optimized/rev-0c309e7/stoneplus.com/wp-content/uploads/2020/06/Rocky-Mountain-Boulder-1-jpg-1000x1000.webp";
+
+const humanChoiceImg = document.querySelector(".human>.img-wrap>img");
+const commputerChoiceImg = document.querySelector(".computer>.img-wrap>img");
+const resultText = document.querySelector(".result");
+const humanScoreText = document.querySelector("#you-score")
+const computerScoreText = document.querySelector("#com-score")
+
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice == 0) {
-        return "rock";
+        choice =  "rock";
     } else if (choice == 1) {
-        return "paper";
+        choice =  "paper";
     } else {
-        return "scissors";
+        choice =  "scissors";
+    }
+
+    displaySelectionImage(choice, commputerChoiceImg);
+    return choice;
+}
+
+
+const selectionButtons = document.querySelector(".buttons")
+selectionButtons.addEventListener("click", (e) => {
+    const computerChoice = getComputerChoice();
+    const humanChoice = e.target.id.toLowerCase();
+    displaySelectionImage(humanChoice, humanChoiceImg);
+    playRound(humanChoice, computerChoice)
+});
+
+function displaySelectionImage(selection, imgElement) {
+    switch(selection) {
+        case "rock":
+            imgElement.setAttribute("src", rockImg);
+            break;
+        case "paper":
+            imgElement.setAttribute("src", paperImg);
+            break;
+        case "scissors": 
+            imgElement.setAttribute("src", scissorsImg);
+            break;
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Enter 'rock' 'paper' or 'scissors: ").toLowerCase();
-    return choice
-}
 
-
-function playRound(humanChoice, computerChoice) {
-    console.log(`YOU -> ${humanChoice} vs COM -> ${computerChoice}`);
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie!");
-    } else if (humanChoice.length > computerChoice.length || humanChoice.length - computerChoice.length == -4 ) {
-        humanScore += 1;
-        console.log("You win!");
-    } else {
+function playRound(you, com) {
+    if (you === com) {
+        resultText.textContent = ("It's a tie👔");
+    } else if (you.length < com.length || com.length - you.length > 1) {
+        resultText.textContent = ("You Lose 🙁");
         computerScore += 1;
-        console.log("You lose!")
+        computerScoreText.textContent = computerScore;
+    } else {
+        resultText.textContent = ("You Win! 🎉");
+        humanScore += 1;
+        humanScoreText.textContent = humanScore;
     }
 }
-
-function playGame() {
-    for (let i = 0; i < 5; i ++) {
-        // console.log(`com score: ${computerScore} : you score: ${humanScore}`);
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-    console.log(`FINAL SCORE \nCOM ${computerScore} : YOU ${humanScore}`);
-}
-
-
-playGame();
