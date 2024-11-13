5059ed2cd0242fa09fc14c18be1648a029f78c92
GiantMango <subway1101@gmail.com>
2024-11-12 16:18:06 -0800

    Change grid, clear canvas, random color all work

--- projects/etch-a-sketch/script.js
+++ projects/etch-a-sketch/script.js
@@ -1,3 +1,63 @@
 const canvas = document.querySelector(".canvas");
-const cell = document.querySelector(".cell");
-const row = document.querySelector(".row");
+const changeGridButton = document.querySelector("button#change-grid");
+const gridSizeInput = document.querySelector("input#grid-size");
+const clearCanvasButton = document.querySelector("button#clear-canvas");
+const randomColorButton = document.querySelector("button#random-mode");
+const defaultGridSize = 10;
+let currentGridSize = defaultGridSize;
+
+createCanvasGrid(defaultGridSize);
+draw();
+
+
+changeGridButton.addEventListener("click", () => {
+    resetCanvas();
+    currentGridSize = gridSizeInput.value
+    createCanvasGrid(currentGridSize);
+    draw();
+})
+
+clearCanvasButton.addEventListener("click", () => {
+    resetCanvas();
+    createCanvasGrid(currentGridSize);
+    draw();
+})
+
+randomColorButton.addEventListener("click", () => {
+    resetCanvas();
+    createCanvasGrid(currentGridSize);
+    draw(1);
+})
+
+function createCanvasGrid(gridSize){
+    for (let i = 0; i < gridSize; i ++) {
+        let newRow = document.createElement("div")
+        newRow.classList.add("row", "flex-row");
+        canvas.appendChild(newRow);
+        for (let j = 0; j < gridSize; j ++) {
+            let newCell = document.createElement("div");
+            newCell.classList.add("cell");
+            newRow.appendChild(newCell);
+        }
+    }
+}
+
+
+function draw(mode = 0) {
+    let cells = document.querySelectorAll(".cell")
+    cells.forEach((e) => {
+        e.addEventListener("mouseover", (e) => {
+            if (mode == 0) {
+                e.target.style.backgroundColor = "black";
+            } else {
+                e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
+            }
+            
+        })
+    })
+}
+
+
+function resetCanvas() {
+    canvas.innerHTML = "";
+}
