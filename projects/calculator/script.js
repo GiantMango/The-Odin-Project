5463d0bca866a7805fb17bb35a5620ea6d58555e
GiantMango <subway1101@gmail.com>
2024-11-20 21:15:26 -0800

    [ver1.0.0] need % to work

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -1,156 +1,278 @@
 const calculatorCanvas = document.querySelector(".calculator");
-// const displayScreen = document.querySelector(".display-screen");
-const curCalcDisplay = document.querySelector("#cur-calc");
-const prevCalcDisplay = document.querySelector("#prev-calc");
+
+const botCalcDisplay = document.querySelector("#cur-calc");
+const topCalcDisplay = document.querySelector("#prev-calc");
 const buttonCanvas = document.querySelector(".button-canvas");
-const accessoryText = ['AC', '+/-', '%'];
-const accessoryID = ['clear-all', 'is-neg', 'percent'];
-const operatorsText = ['÷', 'x', '+', '-', '='];
-const operatorsID = ['divide', 'multiply', 'add', 'subtract', 'equal'];
-const digitsText = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '.', '0', '00'];
-const rowNum = 4;
+const accessoryText = ["AC", "Del", "%"];
+const accessoryID = ["clear-all", "del", "percent"];
+const operatorsText = ["÷", "x", "+", "-"];
+const operatorsID = ["divide", "multiply", "add", "subtract"];
+const digitsText = [
+  "7",
+  "8",
+  "9",
+  "4",
+  "5",
+  "6",
+  "1",
+  "2",
+  "3",
+  ".",
+  "0",
+  "00",
+];
+const rowNum = 5;
 const padNum = 4;
+const opRegex = /[÷x\+\-]/g;
+const priOpRegex = /[÷x]/g;
+var eqPointer = 0;
+var eqStack = [[], [], []];
+var answer = [];
 
-createCalculatorCanvas()
-drawButtons()
+createCalculatorCanvas();
+drawFunctionalButtons();
 
+// UI Functions
 function createCalculatorCanvas() {
-    for (let i = 0; i < 5; i ++) {
-        const newRow = document.createElement("div");
-        newRow.classList.add("flex-row", "row");
-        buttonCanvas.appendChild(newRow);
+  for (let i = 0; i < rowNum; i++) {
+    const newRow = document.createElement("div");
+    newRow.classList.add("flex-row", "row");
+    buttonCanvas.appendChild(newRow);
 
+    for (let j = 0; j < padNum; j++) {
+      const newCell = document.createElement("div");
+      const newButton = document.createElement("button");
+      newCell.classList.add("button-wrappers");
 
-        for (let j = 0; j < 4; j ++) {
-            const newCell = document.createElement("div");
-            const newButton = document.createElement("button");
-            newCell.classList.add("button-wrappers");
-            
-            if (j === 3) {
-                newButton.classList.add("operators");
-            } else if (i == 0) {
-                newButton.classList.add("accessory");
-            } else {
-                newButton.classList.add("digits")
-            }
-            
-            newRow.appendChild(newCell);
-            newCell.appendChild(newButton);
-        }
+      if (j == padNum - 1 && i == rowNum - 1) {
+        newButton.classList.add("equal");
+        newButton.innerHTML = "=";
+        newButton.setAttribute("id", "equal");
+        newButton.onclick = () => equal();
+      } else if (j == padNum - 1) {
+        newButton.classList.add("operators");
+      } else if (i == 0) {
+        newButton.classList.add("accessory");
+      } else {
+        newButton.classList.add("digits");
+      }
 
+      newRow.appendChild(newCell);
+      newCell.appendChild(newButton);
     }
-
+  }
 }
 
-function drawButtons() {
-    const accessoryButton = document.querySelectorAll(".accessory");
-    for (let i = 0; i < accessoryButton.length; i ++) {
-        accessoryButton[i].innerHTML = accessoryText[i];
-        accessoryButton[i].setAttribute("id", accessoryID[i]);
-    }
-    const operatorsButton = document.querySelectorAll(".operators");
-    for (let i = 0; i < operatorsButton.length; i ++) {
-        operatorsButton[i].innerHTML = operatorsText[i];
-        operatorsButton[i].setAttribute("id", operatorsID[i]);
-    }
+function drawFunctionalButtons() {
+  const accessoryButton = document.querySelectorAll(".accessory");
+  for (let i = 0; i < accessoryButton.length; i++) {
+    accessoryButton[i].innerHTML = accessoryText[i];
+    accessoryButton[i].setAttribute("id", accessoryID[i]);
+  }
 
-    const digitsButtons = document.querySelectorAll(".digits");
-    for (let i = 0; i < digitsButtons.length; i ++) {
-        digitsButtons[i].innerHTML = digitsText[i];
-    }
+  accessoryButton[0].onclick = () => clearDisplay();
+  accessoryButton[1].onclick = () => deleteText();
+  accessoryButton[2].onclick = () => percentage();
+
+  const operatorsButton = document.querySelectorAll(".operators");
+  for (let i = 0; i < operatorsButton.length; i++) {
+    operatorsButton[i].innerHTML = operatorsText[i];
+    operatorsButton[i].setAttribute("id", operatorsID[i]);
+    operatorsButton[i].onclick = () => appendToDisplay(operatorsText[i]);
+  }
+
+  const digitsButtons = document.querySelectorAll(".digits");
+  for (let i = 0; i < digitsButtons.length; i++) {
+    digitsButtons[i].innerHTML = digitsText[i];
+    digitsButtons[i].onclick = () => appendToDisplay(digitsText[i]);
+  }
 }
 
-var isFirst = 1;
-var answer = 0;
-var a = null;
-var aText = '';
-var b = null;
-var bText = '';
-var op = null;
-var opCount = 0;
-var equation = ''
+function appendToDisplay(s) {
+  formatEqStack(s);
+  console.log("eqStack: ", eqStack);
 
-const buttons = document.querySelectorAll("button")
-buttons.forEach((e) => {
-    e.addEventListener("click", (e) => {
-        
-        if ("digits" == e.target.classList) {
-            if (a == null) {
-                aText += e.target.textContent;
-            } else {
-                bText += e.target.textContent
-            }
+  // redraw display
+  renewDisplay();
+}
 
-        } else if ("operators" == e.target.classList) {
+function calculate() {
+  // console.log("CALCULATE()");
+  // console.log("eqStack: ", eqStack);
+  let eqStackFlat = eqStack.map((e) => e.join(""));
+  // console.log("flat join eqStack: ", eqStackFlat);
 
-            if (aText != '') {
-                a = parseInt(aText);
-                aText = '';
-            } else if (bText != '') {
-                b = parseInt(bText);
-                bText = '';
-            }
+  let isEmpty = eqStackFlat.reduce((acc, cur) => {
+    return acc || cur == "";
+  }, false);
 
-            if ('equal' != e.target.getAttribute("id")) {
-                op = e.target.getAttribute("id");
-                opCount = 1;
-            }
-        } else if (opCount == 1) {
-            a = operate(op, a, b);
-            b = null;
-            bText = '';
-            console.log(a)
-            opCount = 0;
-        }
+  if (isEmpty) {
+    answer = parseFloat(eqStackFlat[0]);
+  } else {
+    answer = operate(
+      eqStackFlat[1],
+      parseFloat(eqStackFlat[0]),
+      parseFloat(eqStackFlat[2])
+    );
+  }
 
-        if (e.target.getAttribute("id") == 'equal') {
-            a = operate(op, a, b);
-            b = null;
-            bText = '';
-            op = null;
-            
-            prevCalcDisplay.textContent = equation;
-            equation = a;
-            curCalcDisplay.textContent = a;
-            
-        } else {
-            equation += e.target.textContent;
-            curCalcDisplay.textContent = equation;
-        }
+  topCalcDisplay.textContent = eqStackFlat.join("");
+  botCalcDisplay.textContent = answer;
 
-        if ('clear-all' == e.target.getAttribute("id")) {
-            clearDisplay();
-        }
+  return answer;
+}
 
+function formatEqStack(s) {
+  eqStack[eqPointer].push(s);
+  switch (eqPointer) {
+    // num 1
+    case 0:
+      // console.log("IN CASE 0");
+      if (eqStack[0].length == 1 && operatorsText.includes(s) && s != "-") {
+        // console.log("IN CASE 0, FIRST IF");
+        eqStack[0].pop();
+      } else if (
+        eqStack[0][0] == "-" &&
+        operatorsText.includes(eqStack[0][1])
+      ) {
+        // console.log("IN CASE 0, FIRST ELSE");
+        eqStack[0].pop();
+      } else if (
+        eqStack[0].slice(0, eqStack[0].length - 1).includes(".") &&
+        s == "."
+      ) {
+        // console.log("IN CASE 0, SECOND ELSE");
+        eqStack[0].pop();
+      } else if (eqStack[0][eqStack[0].length - 1] == "%") {
+        console.log("in percentage");
+        eqPointer += 1;
+      }
 
-    })
-})
+      if (
+        ((eqStack[0][0] == "-" && eqStack[0].length > 1) ||
+          (eqStack[0][0] != "-" && eqStack[0].length > 0)) &&
+        operatorsText.includes(s)
+      ) {
+        // console.log("IN CASE 0, SECOND IF");
+        eqPointer += 1;
+        let op = eqStack[0].pop();
+        eqStack[1].push(op);
+      }
+      break;
 
+    // operator
+    case 1:
+      // console.log("IN CASE 1");
+      if (eqStack[1].length > 1 && operatorsText.includes(s) && s != "-") {
+        // console.log("IN CASE 1 IF");
+        eqStack[1].shift();
+      } else if (s == "-" || digitsText.includes(s)) {
+        eqPointer += 1;
+        let next = eqStack[1].pop();
+        eqStack[2].push(next);
+        // console.log("sending to case 2");
+      }
+      break;
 
+    // num2
+    case 2:
+      if (eqStack[2].length == 1 && operatorsText.includes(s) && s != "-") {
+        // console.log("IN CASE 2, FIRST IF");
+        eqStack[2].pop();
+      } else if (
+        eqStack[2][0] == "-" &&
+        operatorsText.includes(eqStack[2][1])
+      ) {
+        // console.log("IN CASE 2, FIRST ELSE");
+        eqStack[2].pop();
+      } else if (
+        eqStack[2].slice(0, eqStack[2].length - 1).includes(".") &&
+        s == "."
+      ) {
+        // console.log("IN CASE 0, SECOND ELSE");
+        eqStack[2].pop();
+      }
+
+      if (
+        ((eqStack[2][0] == "-" && eqStack[2].length > 1) ||
+          (eqStack[2][0] != "-" && eqStack[2].length > 0)) &&
+        operatorsText.includes(s)
+      ) {
+        // console.log("IN CASE 2, SECOND IF");
+        let op = eqStack[2].pop();
+        answer = calculate();
+        eqStack = [[], [], []];
+        eqStack[1].push(op);
+        eqStack[0].push(answer);
+      }
+      break;
+  }
+  // console.log("eqPointer: ", eqPointer);
+  // console.log("eqStack: ", eqStack);
+}
+
+function equal() {
+  answer = calculate();
+  topCalcDisplay.textContent = eqStack.flat().join("");
+  botCalcDisplay.textContent = answer;
+  resetEqStack();
+  eqStack[0] = answer.toString().split("");
+}
+
+function percentage() {
+  if (eqPointer != 1 && !eqStack[eqPointer].includes("%")) {
+    eqStack[eqPointer].push("%");
+  }
+  renewDisplay();
+}
+
+// Helper Functions
 function operate(op, a, b) {
-    switch (op) {
-        case 'add':
-            return a + b;
-        case 'subtract':
-            return a - b;
-        case 'multiply':
-            console.log("multiply: ", a, b)
-            return a * b;
-        case 'divide':
-            return a / b;
-    }
+  // console.log("OPERATE (OP, a, b) = (", op, a, b, ")");
+  switch (op) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "x":
+      return a * b;
+    case "÷":
+      if (b == 0) {
+        throw new Error("Can't divided by 0");
+      } else {
+        return a / b;
+      }
+  }
 }
-
 
 function clearDisplay() {
-    curCalcDisplay.innerHTML = ''
-    prevCalcDisplay.innerHTML = ''
-    answer = 0;
-    a = null;
-    aText = '';
-    b = null;
-    bText = '';
-    op = null;
-    opCount = 0;
-    equation = ''
+  resetEqStack();
+  resetAnswer();
+  botCalcDisplay.textContent = "";
+  topCalcDisplay.textContent = "";
+}
+
+function deleteText() {
+  while (eqStack[eqPointer].length == 0) {
+    eqPointer -= 1;
+    if (eqPointer < 0) {
+      eqPointer = 0;
+      break;
+    }
+  }
+  eqStack[eqPointer].pop();
+  renewDisplay();
+}
+
+function renewDisplay() {
+  botCalcDisplay.textContent = eqStack.flat().join("");
+}
+
+function resetEqStack() {
+  eqPointer = 0;
+  eqStack = [[], [], []];
+}
+
+function resetAnswer() {
+  answer = [];
 }
